e125f8bc4f1bc5284db978de2bd9fb32eb457c3b
allema_s <sebastien.a.consulting@gmail.com>
2014-03-14 16:40:19 +0100

    Nettoyage ArrayMatrix

--- bigmat/headers/ArrayMatrix.h
+++ bigmat/headers/ArrayMatrix.h
@@ -7,13 +7,6 @@ template <typename T>
 class ArrayMatrix : public MatrixEngine<T>
 {
 private:
-	struct MatCell
-	{
-		unsigned int x;
-		unsigned int y;
-		MatCell(unsigned int x, unsigned int y) : x(x), y(y) {};
-	};
-
 	int **_matrix;
 	unsigned int _width;
 	unsigned int _height;
@@ -38,9 +31,8 @@ public:
 
 	void set(unsigned int x, unsigned int y, T v)
 	{
-		if(x > _width && y > _height)
-			std::cout << "Error : matrix overflow" << std::endl;
-		_matrix[x][y] = v;
+		if(x < _width && y < _height)
+			_matrix[x][y] = v;
 	};
 
 	unsigned int getWidth(){ return width; };
