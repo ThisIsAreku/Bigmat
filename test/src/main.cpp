f6e5b263969c75e86b0a031470dc450f98c7e23f
Alexandre Boucey <alexandre.boucey@gmail.com>
2014-03-14 16:54:17 +0100

    Expériementations

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+#include "MathMatrix.h"
+#include "HashMapMatrix.h"
+
 int main()
 {
+	MatrixEngine<int> *m1 = new HashMapMatrix<int>;
+	MatrixEngine<int> *m2 = new HashMapMatrix<int>;
+
+	m1->set(0, 0, 1);
+	m1->set(1, 0, 2);
+	m1->set(2, 0, 3);
+
+	m2->set(0, 0, 9);
+	m2->set(1, 0, 8);
+	m2->set(2, 0, 7);
+
+	*m1 += *m2;
 }
