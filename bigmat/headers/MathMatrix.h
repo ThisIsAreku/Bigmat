d6105c037fd6573e6a45e21faf665946a8d1841c
Alexandre Boucey <alexandre.boucey@gmail.com>
2014-03-14 17:40:31 +0100

    Allow unsecure operation

--- bigmat/headers/MathMatrix.h
+++ bigmat/headers/MathMatrix.h
@@ -3,12 +3,26 @@
 
 #include "MatrixEngine.h"
 
+// false : permet l'ajout de matrices de différentes tailles
+static bool secured = true;
+
 template <typename K, typename S>
 MatrixEngine<K> &operator +=(MatrixEngine<K>& a, MatrixEngine<S>& b){
-	if(a.getWidth() != b.getWidth() || a.getHeight() != b.getHeight())
+	if(secured && (a.getWidth() != b.getWidth() || a.getHeight() != b.getHeight()))
 		return a;
-	for(unsigned int i = 0; i < a.getWidth(); i++)
-		for(unsigned int j = 0; j < a.getHeight(); j++)
+
+	unsigned int maxWidth = a.getWidth();
+	unsigned int maxHeight = a.getHeight();
+
+	if(!secured){
+		if(b.getWidth() > maxWidth)
+			maxWidth = b.getWidth();
+		if(b.getHeight() > maxHeight)
+			maxHeight = b.getHeight();
+	}
+
+	for(unsigned int i = 0; i < maxWidth; i++)
+		for(unsigned int j = 0; j < maxHeight; j++)
 			a.set(i, j, a.get(i, j)+b.get(i, j));
 	return a;
 }
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 void dispMat(MatrixEngine<int> *m)
 {
 	cout << m->getWidth() << "x" << m->getHeight() << endl;
-	for(unsigned int i = 0; i < m->getHeight(); i++)
+	for(unsigned int j = 0; j < m->getHeight(); j++)
 	{
-		for(unsigned int j = 0; j < m->getWidth(); j++)
+		for(unsigned int i = 0; i < m->getWidth(); i++)
 		{
 			cout << m->get(i, j) << '\t';
 		}
@@ -33,8 +33,8 @@ int main()
 	m2.set(1, 0, 75);
 	m2.set(2, 0, 8);
 	m2.set(2, 1, 5);
-	m2.set(2, 2, 12);
-	
+	//m2.set(2, 2, 12);
+
 	cout << "m1" << endl;
 	dispMat(&m1);
 
@@ -44,4 +44,14 @@ int main()
 	m1 += m2;
 	cout << "m1" << endl;
 	dispMat(&m1);
+
+	secured = false;
+
+	m1 += m2;
+	cout << "m1" << endl;
+	dispMat(&m1);
+
+	m2 += m1;
+	cout << "m2" << endl;
+	dispMat(&m2);
 }
