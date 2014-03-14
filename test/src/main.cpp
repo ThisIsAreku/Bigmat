9eab0223f74b51fdcae365d25a820dc2964931c9
Alexandre Boucey <alexandre.boucey@gmail.com>
2014-03-14 17:28:34 +0100

    Ajout de tests

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -3,18 +3,45 @@
 #include "MathMatrix.h"
 #include "HashMapMatrix.h"
 
+using namespace std;
+
+void dispMat(MatrixEngine<int> *m)
+{
+	cout << m->getWidth() << "x" << m->getHeight() << endl;
+	for(unsigned int i = 0; i < m->getHeight(); i++)
+	{
+		for(unsigned int j = 0; j < m->getWidth(); j++)
+		{
+			cout << m->get(i, j) << '\t';
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
 int main()
 {
-	MatrixEngine<int> *m1 = new HashMapMatrix<int>;
-	MatrixEngine<int> *m2 = new HashMapMatrix<int>;
+	HashMapMatrix<int> m1;
+	HashMapMatrix<int> m2;
 
-	m1->set(0, 0, 1);
-	m1->set(1, 0, 2);
-	m1->set(2, 0, 3);
+	m1.set(0, 0, 1);
+	m1.set(1, 0, 2);
+	m1.set(2, 0, 3);
+	m1.set(2, 2, 5);
 
-	m2->set(0, 0, 9);
-	m2->set(1, 0, 8);
-	m2->set(2, 0, 7);
+	m2.set(0, 0, 12);
+	m2.set(1, 0, 75);
+	m2.set(2, 0, 8);
+	m2.set(2, 1, 5);
+	m2.set(2, 2, 12);
+	
+	cout << "m1" << endl;
+	dispMat(&m1);
 
-	*m1 += *m2;
+	cout << "m2" << endl;
+	dispMat(&m2);
+
+	m1 += m2;
+	cout << "m1" << endl;
+	dispMat(&m1);
 }
