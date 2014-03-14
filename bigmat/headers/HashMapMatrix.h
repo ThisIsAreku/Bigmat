2158725e0e6eba0bf951b86d286ec54aaa1afaee
Alexandre Boucey <alexandre.boucey@gmail.com>
2014-03-14 16:57:23 +0100

    Commentaires

--- bigmat/headers/HashMapMatrix.h
+++ bigmat/headers/HashMapMatrix.h
@@ -4,16 +4,21 @@
 #include <map>
 #include "MatrixEngine.h"
 
+// classe qui utilise une liste de clée-valeurs. une clée est une struct MatCell(x, y), la valeur est la case de la matrice
 template <typename T>
 class HashMapMatrix : public MatrixEngine<T>
 {
 private:
+	// la structure permet d'écrire rapidement une coordonné x,y
 	struct MatCell
 	{
 		unsigned int x;
 		unsigned int y;
 		MatCell(unsigned int x, unsigned int y) : x(x), y(y) {};
 	};
+	// cette classe permet de comparer deux coordonnés,
+	// true si a doit etre avant b
+	// false si b doit etre avant a
 	class compareKeyPair
 	{
 	public:
@@ -33,14 +38,16 @@ private:
 	unsigned int width;
 	unsigned int height;
 public:
-	HashMapMatrix(){};
-	~HashMapMatrix();
 	
+	// récupère la case à l'indexe x, y
+	// renvoit 0 si vide ou hors limite
 	T get(unsigned int x, unsigned int y)
 	{
 		return _matrix[MatCell(x, y)];
 	};
 
+	// défini la case à l'index x, y à la valeur v
+	// agrandit la taille stocké si l'index est plus grand (extensible)
 	void set(unsigned int x, unsigned int y, T v)
 	{
 		if(x > width)
