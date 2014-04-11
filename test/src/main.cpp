4e385f2e97a4b432461a56a84d46ced2a836e7e8
Alexandre Boucey <alexandre.boucey@gmail.com>
2014-04-11 11:19:39 +0200

    Petits test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -2,12 +2,14 @@
 
 #include "MathMatrix.h"
 #include "matrix/HashMapMatrix.h"
+#include "matrix/ArrayMatrix.h"
 
 using namespace std;
 
 void dispMat(MatrixEngine<int> *m)
 {
     cout << m->getWidth() << "x" << m->getHeight() << endl;
+    cout << "Pointer: " << m << endl;
     for(unsigned int j = 0; j < m->getHeight(); j++)
     {
         for(unsigned int i = 0; i < m->getWidth(); i++)
@@ -24,6 +26,8 @@ int main()
     HashMapMatrix<int> m1;
     HashMapMatrix<int> m2;
 
+    ArrayMatrix<int> a1(3, 3);
+
     m1.set(0, 0, 1);
     m1.set(1, 0, 2);
     m1.set(2, 0, 3);
@@ -34,6 +38,14 @@ int main()
     m2.set(2, 0, 8);
     m2.set(2, 1, 5);
     //m2.set(2, 2, 12);
+
+    a1.set(1, 1, 200);
+    a1.set(0, 0, 200);
+    a1.set(2, 1, 200);
+    //a1.set(4, 4, 5);
+
+    cout << "a1" << endl;
+    dispMat(&a1);
 
     cout << "m1" << endl;
     dispMat(&m1);
@@ -54,4 +66,21 @@ int main()
     m2 += m1;
     cout << "m2" << endl;
     dispMat(&m2);
+
+    m2 += a1;
+    cout << "m2" << endl;
+    dispMat(&m2);
+
+
+    HashMapMatrix<int> m3(m2);
+    cout << "recopie m3 = m2" << endl;
+    dispMat(&m3);
+
+    ArrayMatrix<int> a2(a1);
+    cout << "recopie a2 = a1" << endl;
+    dispMat(&a2);
+
+    ArrayMatrix<int> a3(m3);
+    cout << "recopie a3 = m3" << endl;
+    dispMat(&a3);
 }
