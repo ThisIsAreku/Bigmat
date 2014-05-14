c8520ec4c9e4d5a937f811eeb24df295b12245eb
Alexandre Boucey <alexandre.boucey@gmail.com>
2014-05-14 13:05:30 +0200

    Corrections dans MathMatrix

--- bigmat/headers/MathMatrix.h
+++ bigmat/headers/MathMatrix.h
@@ -9,7 +9,22 @@
    * \date 13 Mai 2014
  */
 
-class MathMatrix;
+class MathMatrix{
+public:
+   
+    template <typename K>
+    static MatrixEngine<K> inversion(MatrixEngine<K> &a);
+    
+    template <typename K>
+    static MatrixEngine<K> complementAlgebrique(MatrixEngine<K> &a);
+    
+    template <typename K>
+    static MatrixEngine<K> transpose(MatrixEngine<K> &a);
+    
+    template <typename K>
+    static int det(MatrixEngine<K> &a);
+
+};
 
 MatrixEngine<bool> &operator +=(MatrixEngine<bool> &a, MatrixEngine<bool> &b)
 {
@@ -18,7 +33,7 @@ MatrixEngine<bool> &operator +=(MatrixEngine<bool> &a, MatrixEngine<bool> &b)
     
     for(unsigned int i = 0; i < maxWidth; i++){
         for(unsigned int j = 0; j < maxHeight; j++){
-            a.set(i, j, a.get(i, j) + b.get(i, j));
+            a.set(i, j, a.get(i, j) || b.get(i, j));
         }
     }
     return a;
@@ -82,14 +97,14 @@ MatrixEngine<bool> &operator *=(MatrixEngine<bool> &a, MatrixEngine<bool> &b){
     a.setHeight(maxHeight);
     a.setWidth(maxWidth);
     
-    K tmp =0;
-    K value = 0;
+    bool tmp = false;
+    bool value = false;
     
     for(unsigned int i = 0; i < maxHeight; i++){
         for(unsigned int j = 0; j < maxWidth; j++){
             for(unsigned int k = 0; k < maxHeight; k++){
-                tmp = a.get(k,i) + b.get(j,k);
-                value += tmp;
+                tmp = a.get(k,i) || b.get(j,k);
+                value = value || tmp;
             }
             a.set(i,j,value);
             value = 0;
@@ -122,90 +137,56 @@ MatrixEngine<K> &operator *=(MatrixEngine<K> &a, MatrixEngine<K> &b){
     return a;
 }
 
-MatrixEngine<bool> &operator /=(MatrixEngine<bool> &a, MatrixEngine<bool> &b){
-    unsigned int maxWidth = b.getWidth();
-    unsigned int maxHeight = a.getHeight();
+MatrixEngine<bool> &operator /=(MatrixEngine<bool> &a, MatrixEngine<bool> &b){    
+    return a;
+}
+
+
+
+template <typename K>
+MatrixEngine<K> &operator /=(MatrixEngine<K> &a, MatrixEngine<K> &b){ 
+    a *= MathMatrix::inversion(b);    
+    return a;
+}
+   
+template <typename K>
+MatrixEngine<K> MathMatrix::inversion(MatrixEngine<K> &a){
     
-    b = MathMatrix::inversion(b);
+    int det = det(a);
     
-    a *= b;
+    MatrixEngine<K> aPrime = complementAlgebrique(a);
+    MatrixEngine<K> aPrimeTrans = transpose(aPrime);
+    a = (aPrimeTrans *= (1/det));
     
     return a;
 }
 
 template <typename K>
-MatrixEngine<K> &operator /=(MatrixEngine<K> &a, MatrixEngine<K> &b){
-    unsigned int maxWidth = b.getWidth();
-    unsigned int maxHeight = a.getHeight();
+MatrixEngine<K> MathMatrix::complementAlgebrique(MatrixEngine<K> &a){
     
-    b = MathMatrix::inversion(b);
-    
-    a *= b;
     
     return a;
 }
 
-/*class MathMatrix{
-public:
-   
-    template <typename K>
-    static MatrixEngine<K> inversion(MatrixEngine<K> &a){
-        
-        int det = det(a);
-        
-        aPrime = complementAlgebrique(a);
-        aPrimeTrans = transpose(aPrime);
-        a = aTrans *= (1/det);
-        
-        return a;
-    }
+template <typename K>
+MatrixEngine<K> MathMatrix::transpose(MatrixEngine<K> &a){
     
-    template <typename K>
-    static MatrixEngine<K> inversion(MatrixEngine<K> &a){
-        
-        int det = det(a);
-        
-        aPrime =
-        aPrimeTrans = transpose(aPrime);
-        a = aTrans *= (1/det);
-        
-        return a;
-    }
     
-    template <typename K>
-    MatrixEngine<k> complementAlgebrique(MatrixEngine<K> &a){
-        
-        
-        return a;
-    }
     
-    template <typename K>
-    MatrixEngine<k> transpose(MatrixEngine<K> &a){
-        
-        
-        
-        return a;
-    }
- 
-     
-    // La matrice doit etre carré et triangulaire
-     
-     
-
+    return a;
+}
+  
+template <typename K>
+int MathMatrix::det(MatrixEngine<K> &a){
+    int det = a.get(0, 0);
     
-    template <typename K>
-    int det(MatrixEngine<K> &a){
-        int det = a[0,0];
-        
-        if(a.getHeight ==  a.getWidth){
-            for(int i =1; i < a.getWidth; i++){
-                det *= a[i,i];
-            }
+    if(a.getHeight() ==  a.getWidth()){
+        for(int i =1; i < a.getWidth(); i++){
+            det *= a.get(i, i);
         }
-        
-        return det;
     }
-
-};*/
+    
+    return det;
+}
 
 #endif
